e7234d774778549e829a5c476e59420d9f278ee0
Tihomir Georgiev <georgiev.tm@abv.bg>
2022-08-27 16:38:13 +0200

    Added SeedCategories

--- NewsRoom/Infrastructure/ApplicationBuilderExtensions.cs
+++ NewsRoom/Infrastructure/ApplicationBuilderExtensions.cs
@@ -2,6 +2,8 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using NewsRoom.Data;
+using NewsRoom.Data.Models;
+using System.Linq;
 
 namespace NewsRoom.Infrastructure
 {
@@ -15,10 +17,27 @@ namespace NewsRoom.Infrastructure
             var data = scopedServices.ServiceProvider.GetService<NewsRoomDbContext>();
 
             data.Database.Migrate();
+
+            SeedCategories(data);
+
             return app;
         }
 
-       
+       private static void SeedCategories(NewsRoomDbContext data)
+        {
+            if (data.Categories.Any())
+            {
+                return;
+            }
+
+            data.Categories.AddRange(new[]
+            {
+                new Category { Name = "България"},
+                new Category { Name = "Свят"},
+            });
+
+            data.SaveChanges();
+        }
 
     }
 }

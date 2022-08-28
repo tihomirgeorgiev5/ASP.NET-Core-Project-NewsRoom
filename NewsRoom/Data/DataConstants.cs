134b929d98105556f1ebe485c689c2d718b9b63b
Tihomir Georgiev <georgiev.tm@abv.bg>
2022-08-28 17:31:34 +0200

    Added DataConstants things

--- NewsRoom/Data/DataConstants.cs
+++ NewsRoom/Data/DataConstants.cs
@@ -3,8 +3,14 @@
     public class DataConstants
     {
         public const int NewsAreaMaxLength = 30;
+        public const int NewsAreaMinLength = 2;
 
         public const int NewsTitleMaxLength = 50; 
-        public const int NewsDescriptionMaxLength = 10000; 
+        public const int NewsTitleMinLength = 5; 
+
+        public const int NewsDescriptionMaxLength = 10000;
+        public const int NewsDescriptionMinLength = 10;
+
+        public const int NewsDateMaxLength = 01-01-2025;
     }
 }
--- NewsRoom/Models/News/AddNewsFormModel.cs
+++ NewsRoom/Models/News/AddNewsFormModel.cs
@@ -1,21 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using static NewsRoom.Data.DataConstants;
 
 namespace NewsRoom.Models.News
 {
     public class AddNewsFormModel
     {
-     
+        [Required]
+        [MaxLength(NewsAreaMaxLength)]
+        [MinLength(NewsAreaMinLength)]
+
         public string Area { get; init; }
-     
+
+        [Required]
+        [MaxLength(NewsTitleMaxLength)]
+        [MinLength(NewsTitleMinLength)]
         public string Title { get; init; }
 
+        [Required]
+        [MinLength(NewsDescriptionMinLength)]
         public string Description { get; init; }
 
         [Display(Name = "Image URL")]
+        [Required]
+        [Url]
         public string ImageUrl { get; init; }
 
+        [MaxLength(NewsDateMaxLength)]
         public DateTime Date { get; init; }
 
         [Display(Name = "Category")]

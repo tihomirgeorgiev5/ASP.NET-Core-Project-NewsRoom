b5dc4775937dd63af8c058e7962c98f23278a4dc
Tihomir Georgiev <georgiev.tm@abv.bg>
2022-08-28 08:47:26 +0200

    Added bootstrap in Add Views

---
+++ NewsRoom/Controllers/NewsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NewsRoom.Models.News;
+
+namespace NewsRoom.Controllers
+{
+    public class NewsController : Controller
+    {
+        public IActionResult Add() => View();
+
+        [HttpPost]
+
+        public IActionResult Add (AddNewsFormModel aNews)
+        {
+            return View();
+        }
+    }
+}
---
+++ NewsRoom/Models/News/AddNewsFormModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace NewsRoom.Models.News
+{
+    public class AddNewsFormModel
+    {
+     
+        public string Area { get; init; }
+     
+        public string Title { get; init; }
+
+        public string Description { get; init; }
+
+        public string ImageUrl { get; init; }
+
+        public DateTime Date { get; init; }
+
+        public int CategoryId { get; init; }
+    }
+}

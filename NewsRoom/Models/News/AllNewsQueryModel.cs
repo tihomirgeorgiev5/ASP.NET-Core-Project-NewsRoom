3ff9cdef4001aacbd926282f07c709e1a58db6c5
Tihomir Georgiev <georgiev.tm@abv.bg>
2022-09-28 22:09:08 +0200

    added changes till now

---
+++ NewsRoom/Models/News/AllNewsQueryModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewsRoom.Models.News
+{
+    public class AllNewsQueryModel
+    {
+    }
+}

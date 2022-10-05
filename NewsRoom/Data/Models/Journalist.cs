08c6f78de5d2810ac97b9ad3d4acf3fcd4cdc858
Tihomir Georgiev <georgiev.tm@abv.bg>
2022-10-05 19:41:04 +0200

    create class Journalist

---
+++ NewsRoom/Data/Models/Journalist.cs
@@ -0,0 +1,16 @@
+﻿
+namespace NewsRoom.Data.Models
+{
+    public class Journalist
+    {
+        public int Id { get; init; }
+
+        public string Name { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string UserId { get; set; }
+
+      
+    }
+}

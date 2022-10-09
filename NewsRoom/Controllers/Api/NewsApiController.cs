f3827d3803d598ff49cd98d0476847ecd6b15243
Tihomir Georgiev <georgiev.tm@abv.bg>
2022-10-09 18:18:45 +0200

    added NewsApiController

---
+++ NewsRoom/Controllers/Api/NewsApiController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NewsRoom.Data;
+using System.Collections;
+using System.Linq;
+
+namespace NewsRoom.Controllers.Api
+{
+    [ApiController]
+    [Route("api/news")]
+    public class NewsApiController : ControllerBase
+    {
+        private readonly NewsRoomDbContext data;
+
+        public NewsApiController(NewsRoomDbContext data)
+            => this.data = data;
+
+        [HttpGet]
+        public IEnumerable GetNews()
+        {
+            return this.data.News.ToList();
+        }
+    }
+}

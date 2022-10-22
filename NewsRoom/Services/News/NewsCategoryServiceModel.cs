ab49537bf18e7e68b447b17c5dde7dc0c811b692
Tihomir Georgiev <georgiev.tm@abv.bg>
2022-10-22 12:17:10 +0200

    undo changes, because collapsed all aplication after my last changes

---
+++ NewsRoom/Services/News/NewsCategoryServiceModel.cs
@@ -0,0 +1,10 @@
+﻿namespace NewsRoom.Services.News
+{
+    public class NewsCategoryServiceModel
+    {
+        public int Id { get; init; }
+
+        public string Name { get; init; }
+    }
+}
+ 

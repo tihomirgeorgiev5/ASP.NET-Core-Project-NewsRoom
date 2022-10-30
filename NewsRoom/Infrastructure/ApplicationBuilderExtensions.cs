5d299ca0eeaaa794dccc8e71bf43c186f1ae50a7
Tihomir Georgiev <georgiev.tm@abv.bg>
2022-10-30 15:05:04 +0100

    added Admin as a Role

--- NewsRoom/Infrastructure/ApplicationBuilderExtensions.cs
+++ NewsRoom/Infrastructure/ApplicationBuilderExtensions.cs
@@ -1,30 +1,45 @@
 ﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using NewsRoom.Data;
 using NewsRoom.Data.Models;
+using System;
 using System.Linq;
+using System.Threading.Tasks;
+using static NewsRoom.WebConstants;
 
 namespace NewsRoom.Infrastructure
 {
     public static class ApplicationBuilderExtensions
     {
-        public static IApplicationBuilder PrepareDatabase(this IApplicationBuilder app)
+        public static IApplicationBuilder PrepareDatabase(
+            this IApplicationBuilder app)
         {
-            using var scopedServices = app
+            using var serviceScope = app
                 .ApplicationServices.CreateScope();
 
-            var data = scopedServices.ServiceProvider.GetService<NewsRoomDbContext>();
+            var services = serviceScope.ServiceProvider;
 
-            data.Database.Migrate();
+            MigrateDatabase(services);
 
-            SeedCategories(data);
+            SeedCategories(services);
+            SeedAdministrator(services);
 
             return app;
         }
 
-       private static void SeedCategories(NewsRoomDbContext data)
+        private static void MigrateDatabase(IServiceProvider services)
         {
+            var data = services.GetRequiredService<NewsRoomDbContext>();
+
+            data.Database.Migrate();
+        }
+
+        private static void SeedCategories(IServiceProvider services)
+        {
+            var data = services.GetRequiredService<NewsRoomDbContext>();
+
             if (data.Categories.Any())
             {
                 return;
@@ -38,6 +53,46 @@ namespace NewsRoom.Infrastructure
 
             data.SaveChanges();
         }
+
+        private static void SeedAdministrator(IServiceProvider services)
+            
+        {
+            var userManager = services.GetRequiredService<UserManager<User>>();
+            var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+
+            Task
+                .Run(async () =>
+                {
+                    if (await roleManager.RoleExistsAsync(AdministratorRoleName))
+                    {
+                        return;
+                    }
+
+                    var role = new IdentityRole { Name = AdministratorRoleName };
+
+                    await roleManager.CreateAsync(role);
+
+                    const string adminEmail = "admin@nrs.com";
+                    const string adminPassword = "admin1234";
+
+                    var user = new User
+                    {
+                        Email = adminEmail,
+                        UserName = adminEmail,
+                        FullName = "Admin",
+                    };
+
+                    await userManager.CreateAsync(user, adminPassword);
+
+                    await userManager.AddToRoleAsync(user, role.Name);
+                })
+                .GetAwaiter()
+                .GetResult();
+        }
+
+
+
+
 
     }
 }
---
+++ NewsRoom/WebConstants.cs
@@ -0,0 +1,7 @@
+﻿namespace NewsRoom
+{
+    public class WebConstants
+    {
+        public const string AdministratorRoleName = "Administrator";
+    }
+}

4edc7243c7c03541ef9d210c6be9694d834904be
Tihomir Georgiev <georgiev.tm@abv.bg>
2022-11-01 14:32:52 +0100

    added new test

--- NewsRoom.Test/UnitTest1.cs
+++ NewsRoom.Test/UnitTest1.cs
@@ -11,7 +11,12 @@ namespace NewsRoom.Test
             Assert.Equal("2", 2.ToString());
         }
 
-
+        [Theory]
+        [InlineData(2, 3, 5)]
+        public void Test2(int x, int y, int sum)
+        {
+            Assert.Equal(sum, x + y);
+        }
 
       
     }

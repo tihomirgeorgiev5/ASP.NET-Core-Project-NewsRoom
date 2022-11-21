36e52b41bfd009c95dd529dc284d2a5c015db504
Tihomir Georgiev <georgiev.tm@abv.bg>
2022-11-21 21:20:32 +0100

    added class MappingProfile in Infrastructure, to can AutoMapper working corectly

---
+++ NewsRoom/Infrastructure/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using NewsRoom.Models.News;
+using NewsRoom.Services.News.Models;
+
+namespace NewsRoom.Infrastructure
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            this.CreateMap<NewsDetailsServiceModel, NewsFormModel>();
+        }
+    }
+}

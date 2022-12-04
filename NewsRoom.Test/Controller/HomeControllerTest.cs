49933695d0863c2e37bf1c97c9ce9e7fccb087b7
Tihomir Georgiev <georgiev.tm@abv.bg>
2022-12-04 18:23:09 +0100

    delete MapperMock class

--- NewsRoom.Test/Controller/HomeControllerTest.cs
+++ NewsRoom.Test/Controller/HomeControllerTest.cs
@@ -1,16 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
 using NewsRoom.Controllers;
-using NewsRoom.Test.Mocks;
 using Xunit;
 
 namespace NewsRoom.Test.Controller
 {
     public class HomeControllerTest
     {
+        [Fact]
         public void ErrorShouldReturnView()
         {
             // Arrange
-            var homeController = new HomeController(null, MapperMock.Instance, null);
+            var homeController = new HomeController(null, Mock.Of<IMapper>(), null);
 
             // Act
             var result = homeController.Error();

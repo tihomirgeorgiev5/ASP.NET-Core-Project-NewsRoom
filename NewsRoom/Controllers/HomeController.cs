aafb85b77bcf4c77dc581ccda46fb7952f721e44
Tihomir Georgiev <georgiev.tm@abv.bg>
2022-12-08 10:26:56 +0100

    first succesfully test variant, change Error() in HomeController

--- NewsRoom/Controllers/HomeController.cs
+++ NewsRoom/Controllers/HomeController.cs
@@ -47,9 +47,8 @@ namespace NewsRoom.Controllers
             });
         }
         
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error() => 
-           View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+       
+        public IActionResult Error() =>  View();
         
     }
 }

b35195eec55f027b787fd5c71c996efc44d1044f
Tihomir Georgiev <georgiev.tm@abv.bg>
2022-12-08 08:36:01 +0100

    added new void method wih [Theory] to testing

--- NewsRoom.Test/UnitTest1.cs
+++ NewsRoom.Test/UnitTest1.cs
@@ -8,7 +8,15 @@ namespace NewsRoom.Test
         public void Test1()
         {
             Assert.Equal("2", 2.ToString());
-
         }
+
+        [Theory]
+        [InlineData(1,2,3)]
+        public void Test2(int x, int y, int sum)
+        {
+            Assert.Equal(sum, x + y);
+        }
+
+
     }
 }

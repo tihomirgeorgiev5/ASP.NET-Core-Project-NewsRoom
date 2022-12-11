3fde178ef2cc0010c359f4c452c9239d37c27730
Tihomir Georgiev <georgiev.tm@abv.bg>
2022-12-11 08:50:57 +0100

    added integration test for HomeController

--- NewsRoom.Test/Controllers/HomeControllerTest.cs
+++ NewsRoom.Test/Controllers/HomeControllerTest.cs
@@ -1,11 +1,40 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
 using NewsRoom.Controllers;
+using NewsRoom.Models.Home;
+using NewsRoom.Services.News;
+using NewsRoom.Services.Statistics;
+using NewsRoom.Test.Mocks;
 using Xunit;
 
 namespace NewsRoom.Test.Controllers
 {
     public class HomeControllerTest
     {
+        [Fact]
+        public void IndexShouldReturnViewWithCorrectModel()
+        {
+            // Arrange
+            var data = DatabaseMock.Instance;
+            var mapper = MapperMock.Instance;
+
+            var newsService = new NewsService(data, mapper);
+            var statisticsService = new StatisticsService(data);
+
+            var homeController = new HomeController(newsService, statisticsService);
+
+            // Act
+            var result = homeController.Index();
+
+            // Assert
+            Assert.NotNull(result);
+
+            var viewResult = Assert.IsType<ViewResult>(result);
+
+            var model = viewResult.Model;
+
+            var indexViewModel = Assert.IsType<IndexViewModel>(model);
+        }
         [Fact]
         public void ErrorShouldReturnView()
         {
---
+++ NewsRoom.Test/Mocks/MapperMock.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using NewsRoom.Infrastructure;
+
+namespace NewsRoom.Test.Mocks
+{
+    public static class MapperMock
+    {
+        public static IMapper Instance
+        {
+            get 
+            {
+                var mapperConfiguration = new MapperConfiguration(config =>
+                {
+                    config.AddProfile<MappingProfile>();
+                });
+
+                return new Mapper(mapperConfiguration);
+            }
+        }
+    }
+}

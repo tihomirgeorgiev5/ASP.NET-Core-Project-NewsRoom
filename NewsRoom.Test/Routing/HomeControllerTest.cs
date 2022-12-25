182cc6e54c0ef36a7eb7ed72b7a6c27d3cb6665c
Tihomir Georgiev <georgiev.tm@abv.bg>
2022-12-25 17:02:28 +0100

    added new test in Routing for HomeController

---
+++ NewsRoom.Test/Routing/HomeControllerTest.cs
@@ -0,0 +1,25 @@
+﻿using Xunit;
+using MyTested.AspNetCore.Mvc;
+using NewsRoom.Controllers;
+
+namespace NewsRoom.Test.Routing
+{
+    public class HomeControllerTest
+    {
+        [Fact]
+
+        public void IndexRouteShouldBeMapper()
+           => MyRouting
+               .Configuration()
+               .ShouldMap("/")
+               .To<HomeController>(n => n.Index());
+
+        [Fact]
+
+        public void ErrorRouteShouldBeMapper()
+            => MyRouting
+                .Configuration()
+                .ShouldMap("/Home/Error")
+                .To<HomeController>(n => n.Error());
+    }
+}

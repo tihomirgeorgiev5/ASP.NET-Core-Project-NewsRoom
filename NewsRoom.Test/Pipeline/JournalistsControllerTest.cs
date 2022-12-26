0f707a2e8bace716e4b50e684d4bd5ad50940db4
Tihomir Georgiev <georgiev.tm@abv.bg>
2022-12-26 14:11:04 +0100

    added pipeline test for Post method in JournalistsController

--- NewsRoom.Test/Pipeline/JournalistsControllerTest.cs
+++ NewsRoom.Test/Pipeline/JournalistsControllerTest.cs
@@ -1,6 +1,12 @@
 ﻿using MyTested.AspNetCore.Mvc;
 using NewsRoom.Controllers;
+using NewsRoom.Data.Models;
+using NewsRoom.Models.Journalists;
+using NewsRoom.Models.News;
+using System.Linq;
 using Xunit;
+
+using static NewsRoom.WebConstants;
 
 namespace NewsRoom.Test.Pipeline
 
@@ -22,6 +28,49 @@ namespace NewsRoom.Test.Pipeline
                 .AndAlso()
                 .ShouldReturn()
                 .View();
+
+        [Theory]
+        [InlineData("Journalist", "+359888888888")]
+        public void PostBecomeShouldBeForAuthorizedUsersAndReturnRedirectWithValidModel(
+          string journalistName,
+          string phoneNumber)
+          => MyPipeline
+            .Configuration()
+            .ShouldMap(request => request
+               .WithPath("/Journalists/Become")
+               .WithMethod(HttpMethod.Post)
+               .WithFormFields(new 
+                   {
+                   Name = journalistName,
+                   PhoneNumber = phoneNumber
+               
+               
+                   })
+               .WithUser()
+            .WithAntiForgeryToken())
+             .To<JournalistsController>(n => n.Become(new BecomeJournalistFormModel
+             {
+                 Name = journalistName,
+                 PhoneNumber = phoneNumber
+             }))
+             .Which()     
+             .ShouldHave()
+             .ActionAttributes(attributes => attributes
+                 .RestrictingForHttpMethod(HttpMethod.Post)
+                 .RestrictingForAuthorizedRequests())
+             .ValidModelState()
+             .Data(data => data.WithSet<Journalist>(journalists => journalists
+                 .Any(j =>
+                    j.Name == journalistName &&
+                    j.PhoneNumber == phoneNumber &&
+                    j.UserId == TestUser.Identifier)))
+             .TempData(tempData => tempData
+                 .ContainingEntryWithKey(GlobalMessageKey))
+             .AndAlso()
+             .ShouldReturn()
+             .Redirect(redirect => redirect
+                 .To<NewsController>(n => n.All(With.Any<AllNewsQueryModel>())));
+
 
 
 

3aeca6821aef85db92c20bfdc1f8bb47e8131d83
Tihomir Georgiev <georgiev.tm@abv.bg>
2022-12-31 09:33:36 +0100

    added BaseController in Controllers

---
+++ NewsRoom/Controllers/BaseController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NewsRoom.Controllers
+{
+    [Authorize]
+    public class BaseController : Controller
+    {
+
+    }
+}

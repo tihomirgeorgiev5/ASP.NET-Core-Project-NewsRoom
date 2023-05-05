1e6b763c218416ab53f5c4f082ce8d7c94b9b361
Tihomir Georgiev <georgiev.tm@abv.bg>
2023-05-05 17:55:31 +0200

    added Razor view page Logout

--- NewsRoom/Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ NewsRoom/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -3,9 +3,6 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using NewsRoom.Data.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace NewsRoom.Areas.Identity.Pages.Account

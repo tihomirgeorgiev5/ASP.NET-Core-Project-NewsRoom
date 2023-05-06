2f459d3e682f16b864cef2d09a62d33c5afc89bb
Tihomir Georgiev <georgiev.tm@abv.bg>
2023-05-06 19:10:04 +0200

    added Razor View Page Model AccessDenied

---
+++ NewsRoom/Areas/Identity/Pages/Account/AccessDenied.cshtml.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NewsRoom.Areas.Identity.Pages.Account
+{
+    /// <summary>
+    ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+    ///     directly from your code. This API may change or be removed in future releases.
+    /// </summary>
+    public class AccessDeniedModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

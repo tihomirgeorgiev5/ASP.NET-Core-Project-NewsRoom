cbf2e1075612c50dcc36233ef443a0417e6bf009
Tihomir Georgiev <georgiev.tm@abv.bg>
2023-08-13 22:45:45 -0400

    create skeleton for AboutController and models, and added GlobalConstants for it

---
+++ NewsRoom/Areas/Admin/Models/FaqCreateInputViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewsRoom.Areas.Admin.Models
+{
+    public class FaqCreateInputViewModel
+    {
+        public string Question { get; set; }
+
+        public string Answer { get; set; }
+    }
+}
---
+++ NewsRoom/Services/About/IAboutService.cs
@@ -0,0 +1,13 @@
+﻿using NewsRoom.Models.FaqEntity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewsRoom.Services.About
+{
+    public interface IAboutService
+    {
+        Task<FaqViewModel> CreateAsync
+    }
+}

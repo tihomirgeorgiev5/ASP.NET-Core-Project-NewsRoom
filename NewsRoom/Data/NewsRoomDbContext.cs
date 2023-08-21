09386c9f559936641d541a8442c6f5c05218e475
Tihomir Georgiev <georgiev.tm@abv.bg>
2023-08-20 23:39:04 -0400

    added FaqEntity in OnModelCreating successfully

--- NewsRoom/Data/NewsRoomDbContext.cs
+++ NewsRoom/Data/NewsRoomDbContext.cs
@@ -1,6 +1,18 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity; 
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using NewsRoom.Data.Models;
+using NewsRoom.Infrastructure.Data.Common.Models.Contracts;
+using NewsRoom.Infrastructure.Data;
+using NewsRoom.Infrastructure;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using Newtonsoft.Json;
+using System.IO;
 
 namespace NewsRoom.Data
 {
@@ -12,11 +24,16 @@ namespace NewsRoom.Data
         {
         }
 
+        // DbSet represent each table entity in the database
         public DbSet<News> News { get; init; }
 
         public DbSet<Category> Categories { get; init; }
 
         public DbSet<Journalist> Journalists { get; init; }
+
+        public DbSet<FaqEntity> Faqs { get; init; }
+
+
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
@@ -49,7 +66,23 @@ namespace NewsRoom.Data
                 .HasForeignKey<Journalist>(j => j.UserId)
                 .OnDelete(DeleteBehavior.Restrict);
 
+            builder
+                .Entity<FaqEntity>()
+                .HasData(SeedUserData<FaqEntity>(@"DataSeed/faqs.json"));
+
             base.OnModelCreating(builder);
         }
+
+        public List<T> SeedUserData<T>(string filePath) where T : class 
+        {
+            var model = new List<T>();
+            using (StreamReader r = new StreamReader(filePath))
+            {
+                string json = r.ReadToEnd();
+                model = JsonConvert.DeserializeObject<List<T>>(json);
+            }
+            return model;
+
+        } 
     }
 }

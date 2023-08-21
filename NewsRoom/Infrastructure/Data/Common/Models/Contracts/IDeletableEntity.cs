d63de4ad36d1c47c9372fc9b076b6e707eddf601
Tihomir Georgiev <georgiev.tm@abv.bg>
2023-08-20 23:04:35 -0400

    added faqs.json

---
+++ NewsRoom/Infrastructure/Data/Common/Models/Contracts/IDeletableEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewsRoom.Infrastructure.Data.Common.Models.Contracts
+{
+    public interface IDeletableEntity
+    {
+        bool IsDeleted { get; set; }
+
+        DateTime? DeletedOn { get; set; }
+    }
+}

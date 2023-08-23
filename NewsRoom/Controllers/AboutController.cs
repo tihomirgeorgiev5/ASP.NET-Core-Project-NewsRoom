74ceeeea5ebd8a515fd4105bbae980fc4b7a9401
Tihomir Georgiev <georgiev.tm@abv.bg>
2023-08-22 21:53:14 -0400

    added full contained AboutController

--- NewsRoom/Controllers/AboutController.cs
+++ NewsRoom/Controllers/AboutController.cs
@@ -1,12 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using NewsRoom.Models.FaqEntity;
+using NewsRoom.Services.About;
+using System.Threading.Tasks;
 
 namespace NewsRoom.Controllers
 {
     public class AboutController : Controller
     {
-        public IActionResult Index()
+        private readonly IAboutService aboutService;
+
+        public AboutController(IAboutService aboutService)
         {
-            return View();
+            this.aboutService = aboutService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var faq = await aboutService.GetAllFaqsAsync<FaqViewModel>();
+
+            return View(faq);
         }
     }
 }

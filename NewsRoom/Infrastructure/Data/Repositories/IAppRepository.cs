14590dd831aba232981490e0b7f5c0daae91c791
Tihomir Georgiev <georgiev.tm@abv.bg>
2023-08-23 20:19:15 -0400

    added IAppRepository

---
+++ NewsRoom/Infrastructure/Data/Repositories/IAppRepository.cs
@@ -0,0 +1,13 @@
+﻿using NewsRoom.Infrastructure.Data.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewsRoom.Infrastructure.Data.Repositories
+{
+    public interface IAppRepository : IRepository
+    {
+
+    }
+}

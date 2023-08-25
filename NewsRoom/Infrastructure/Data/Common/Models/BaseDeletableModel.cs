5f9c7d67301fdd271f3f1bc0f40602246cd5875a
Tihomir Georgiev <georgiev.tm@abv.bg>
2023-08-24 22:52:24 -0400

    added BaseDeletableModel

---
+++ NewsRoom/Infrastructure/Data/Common/Models/BaseDeletableModel.cs
@@ -0,0 +1,12 @@
+﻿using NewsRoom.Data.Models;
+using NewsRoom.Infrastructure.Data.Common.Models.Contracts;
+using System;
+
+namespace NewsRoom.Infrastructure.Data.Common.Models
+{
+    public abstract class BaseDeletableModel<TKey> : BaseModel<TKey>, IDeletableEntity
+    {
+        public bool IsDeleted { get; set; }
+        public DateTime? DeletedOn { get; set; }
+    }
+}

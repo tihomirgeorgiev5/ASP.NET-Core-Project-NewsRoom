977dbebd8af6ce5d4ae76a8081cb036809e1959e
Tihomir Georgiev <georgiev.tm@abv.bg>
2023-08-24 22:47:45 -0400

    added IAuditInfo

---
+++ NewsRoom/Infrastructure/Data/Common/Models/Contracts/IAuditInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewsRoom.Infrastructure.Data.Common.Models.Contracts
+{
+    public interface IAuditInfo
+    {
+        DateTime CreatedOn { get; set; }
+
+        DateTime? ModifiedOn { get; set; }
+    }
+}

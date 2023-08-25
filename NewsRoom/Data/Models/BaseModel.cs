d96788d76796205d331da51969ee8695a59501b9
Tihomir Georgiev <georgiev.tm@abv.bg>
2023-08-24 22:50:22 -0400

    added BaseModel

---
+++ NewsRoom/Data/Models/BaseModel.cs
@@ -0,0 +1,12 @@
+﻿using NewsRoom.Infrastructure.Data.Common.Models.Contracts;
+using System;
+
+namespace NewsRoom.Data.Models
+{
+    public abstract class BaseModel<TKey> : IAuditInfo
+    {
+        public TKey Id { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime? ModifiedOn { get; set; }
+    }
+}

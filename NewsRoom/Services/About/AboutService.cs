c508c89df921a9a9b8e8801b0105324ca4bc5984
Tihomir Georgiev <georgiev.tm@abv.bg>
2023-08-27 12:00:37 -0400

    implemented deleteId in AboutService

--- NewsRoom/Services/About/AboutService.cs
+++ NewsRoom/Services/About/AboutService.cs
@@ -1,22 +1,66 @@
-﻿using NewsRoom.Areas.Admin.Models;
+﻿using Microsoft.EntityFrameworkCore;
+using NewsRoom.Areas.Admin.Models;
+using NewsRoom.Data;
+using NewsRoom.Data.Models;
+using NewsRoom.Infrastructure.Data.Repositories;
+using NewsRoom.Infrastructure.Data;
+using NewsRoom.Infrastructure.Data.Common;
+using NewsRoom.Infrastructure.Data.Common.Models;
 using NewsRoom.Models.FaqEntity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FaqEntity = NewsRoom.Infrastructure.Data.Common.Models.FaqEntity;
 
 namespace NewsRoom.Services.About
 {
     public class AboutService : IAboutService
     {
-        public Task<FaqViewModel> CreateAsync(FaqCreateInputViewModel model)
+        private readonly IAppRepository _faqRepo;
+        private readonly NewsRoomDbContext _newsRoomDbContext;
+        public AboutService(IAppRepository faqRepo, NewsRoomDbContext _newsRoomDbContext)
         {
-            throw new NotImplementedException();
+            this._faqRepo = faqRepo;
+            this._newsRoomDbContext = _newsRoomDbContext;
         }
 
-        public void DeleteById(int id)
+        public async Task<FaqViewModel> CreateAsync(FaqCreateInputViewModel model)
         {
-            throw new NotImplementedException();
+            var faq = new FaqViewModel
+            {
+                Question = model.Question,
+                Answer = model.Answer,
+            };
+
+            bool isExist = await _newsRoomDbContext.Faqs
+                .Where(f => f.Question == model.Question && f.Answer == model.Answer)
+                .AnyAsync();
+
+            if (isExist)
+            {
+                throw new ArgumentException(string.Format(MessageConstants.Faq.FaqAlreadyExist, model.Question, model.Answer));
+            }
+
+           // var faq1 = await _newsRoomDbContext.Faqs.AddAsync(new FaqEntity()
+           // {
+           //     Answer = model.Answer,
+           //     Question = model.Question,
+           //     CreatedOn = DateTime.Now,
+           //     IsDeleted = false
+           // });
+
+            _newsRoomDbContext.SaveChanges();
+
+            return new FaqViewModel();
+        }
+
+        public void DeleteById(int faqId)
+        {
+            var faqToDelete = _newsRoomDbContext.Faqs.FirstOrDefault(f => f.Id == faqId);
+
+            _newsRoomDbContext.Faqs.Remove(faqToDelete);
+            _newsRoomDbContext.SaveChanges();
         }
 
         public Task<FaqViewModel> EditAsync(FaqEditViewModel model)

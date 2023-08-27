859a10a10f36c50ddcb963eb919333c3cffee5bb
Tihomir Georgiev <georgiev.tm@abv.bg>
2023-08-26 22:53:53 -0400

    added BaseModel in Infrastructure.Data.Model

---
+++ NewsRoom/Infrastructure/Data/Common/Models/BaseModel.cs
@@ -0,0 +1,15 @@
+﻿using NewsRoom.Infrastructure.Data.Common.Models.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewsRoom.Infrastructure.Data.Common.Models
+{
+    public abstract class BaseModel<TKey> : IAuditInfo
+    {
+        public TKey Id { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime? ModifiedOn { get; set; }
+    }
+}

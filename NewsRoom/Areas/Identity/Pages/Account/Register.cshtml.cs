0f3a0ce9d6c920efba48ede4555242294084a0d0
Tihomir Georgiev <georgiev.tm@abv.bg>
2023-11-01 23:42:04 +0100

    install NuGet package for Google.Cloud

--- NewsRoom/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ NewsRoom/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -2,10 +2,8 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.WebUtilities;
 using NewsRoom.Data.Models;
 using System.ComponentModel.DataAnnotations;
-using System.Text;
 using System.Threading.Tasks;
 
 using static NewsRoom.Data.GlobalConstants.User;
